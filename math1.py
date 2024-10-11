a9d5d83537fcb5f1b7f299f5c25e1f4abcd3079e
Kevin Cortez <132033335+Kvncortz@users.noreply.github.com>
2024-10-10 21:52:08 -0400

    Delete math1.py

--- math1.py
+++
@@ -1,7 +0,0 @@
-import math
-
-radius = float(input("Enter a radius of a circle: "))
-
-circumference = 2 * radius * math.pi
-
-print(f"The circumference of the circle is {round(circumference,2)} meters²")
